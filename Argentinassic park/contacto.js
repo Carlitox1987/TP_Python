99d463ffd777e38f81629a8e9203d003bd2a1295
carlos caballero <caballero.carlosadrian@gmail.com>
2022-10-26 20:26:32 -0300

    correcciones varias

--- Argentinassic park/contacto.js
+++ Argentinassic park/contacto.js
@@ -1,43 +1,60 @@
-function enviar()   
-{   
-    let status=validar();
-    if(status===0){
-        confirm("¿Son Correctos los datos a enviar?");
-    }
-    else{
-        alert(status);
-    }
+function enviar() {
+  let status = validar();
+  if (status === 0) {
+    confirm("¿Son Correctos los datos a enviar?");
+  }
+  else {
+    alert("Por favor complete todos los campos requeridos");
+  }
 
 }
-function validar(){
-    let nombre=document.getElementById("Nombre").value;
-    let apellido=document.getElementById("Apellido").value;
-    let fecha=document.getElementById("Fecha").value;
-    let email=document.getElementById("Email").value;
-    let consulta=document.getElementById("Consulta").value;
-    let today = new Date();
- 
-    
-    if (nombre.length === 0)
-  {
-    return "Por favor ingrese su Nombre";
+function validar() {
+  let nombre = document.getElementById("Nombre").value;
+  let apellido = document.getElementById("Apellido").value;
+  let email = document.getElementById("Email").value;
+  let consulta = document.getElementById("Consulta").value;
+  let error = 0;
+  var validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+  if (validEmail.test(email)) {
+    document.getElementById("emailStatus").innerHTML = null;
+  } 
+  else {
+    document.getElementById("emailStatus").innerHTML = 'Introduzca un Email Valido';
+    document.getElementById("emailStatus").style.color = "red";
   }
-  if (apellido.length === 0)
-  {
-    return "Por favor ingrese su Apellido";
+  if (nombre.length === 0) {
+    document.getElementById("nombreStatus").innerHTML = 'Campo Requerido';
+    document.getElementById("nombreStatus").style.color = "red";
+    error = 1;
   }
-  if (email.length === 0)
-  {
-    return "Por favor ingrese su Email";
+  else {
+    document.getElementById("nombreStatus").innerHTML = null;
   }
-  return 0;
+  if (apellido.length === 0) {
+    document.getElementById("apellidoStatus").innerHTML = 'Campo Requerido';
+    document.getElementById("apellidoStatus").style.color = "red";
+    error = 1;
+  }
+  else {
+    document.getElementById("apellidoStatus").innerHTML = null;
+  }
+  if (consulta.length === 0) {
+    document.getElementById("consultaStatus").innerHTML = 'Campo Requerido';
+    document.getElementById("consultaStatus").style.color = "red";
+    error = 1;
+  }
+  else {
+    document.getElementById("consultaStatus").innerHTML = null;
+  }
+  return error;
 }
-function borrar(){
-    if(confirm("Desea borrar todo el formulario?")==true){
-    document.getElementById("Nombre").value="";
-    document.getElementById("Apellido").value="";
-    document.getElementById("Fecha").value="";
-    document.getElementById("Email").value="";
-    document.getElementById("Consulta").value="";
-    }
+function borrar() {
+  if (confirm("Desea borrar todo el formulario?") == true) {
+    document.getElementById("Nombre").value = "";
+    document.getElementById("Apellido").value = "";
+    document.getElementById("Fecha").value = "";
+    document.getElementById("Email").value = "";
+    document.getElementById("Consulta").value = "";
+  }
 }
